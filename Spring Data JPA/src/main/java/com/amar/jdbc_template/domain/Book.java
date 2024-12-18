5c25089829dc47a196a7cc558a129d9da450eb44
amar <amarkodge@gmail.com>
2024-12-18 15:54:35 +0530

    improving code using lombok

--- Spring Data JPA/src/main/java/com/amar/jdbc_template/domain/Book.java
+++ Spring Data JPA/src/main/java/com/amar/jdbc_template/domain/Book.java
@@ -2,12 +2,14 @@ package com.amar.jdbc_template.domain;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-
-import java.util.Objects;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "books")
 public class Book {
 
@@ -20,86 +22,5 @@ public class Book {
     @JoinColumn(name = "author_id")
     private Author author;
 
-    public Book() {
-    }
 
-    public Book(String isbn, String title, Author author) {
-        this.title = title;
-        this.isbn = isbn;
-        this.author = author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Author author) {
-        this.author = author;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return Objects.equals(isbn, book.isbn) && Objects.equals(title, book.title) && Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(isbn, title, author);
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "isbn='" + isbn + '\'' +
-                ", title='" + title + '\'' +
-                ", author=" + author +
-                '}';
-    }
-
-//    public static class BookBuilder{
-//        private String isbn;
-//        private String title;
-//        private Author author;
-//
-//        public BookBuilder isbn(String isbn){
-//            this.isbn = isbn;
-//            return this;
-//        }
-//
-//        public BookBuilder title(String title){
-//            this.title = title;
-//            return this;
-//        }
-//
-//        public BookBuilder author(Author author){
-//            this.author = author;
-//            return this;
-//        }
-//
-//        public Book build() {
-//            return new Book( isbn, title, author);
-//        }
-//    }
-//
-//    public static BookBuilder builder(){
-//        return  new BookBuilder();
-//    }
 }
