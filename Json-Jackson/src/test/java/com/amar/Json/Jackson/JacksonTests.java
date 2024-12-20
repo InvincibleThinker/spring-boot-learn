7ac3eb6f2067deecca6639b9709296ff68b5397f
amar <amarkodge@gmail.com>
2024-12-20 21:04:45 +0530

    Implementing test for creating book using ObjectMapper

---
+++ Json-Jackson/src/test/java/com/amar/Json/Jackson/JacksonTests.java
@@ -0,0 +1,29 @@
+package com.amar.Json.Jackson;
+
+import com.amar.Json.Jackson.domain.Book;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JacksonTests {
+
+    @Test
+    public void testThatObjectMapperCanCreateJsonFromJavaObject() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Book book =
+                Book.builder()
+                        .isbn("978-0-13-478627-5")
+                        .title("The Enigma of Eternity")
+                        .author("Aria Montgomery")
+                        .yearPublished("2005")
+                        .build();
+
+        String result = objectMapper.writeValueAsString(book);
+        assertThat(result)
+                .isEqualTo(
+                        "{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of Eternity\",\"author\":\"Aria"
+                                + " Montgomery\",\"yearPublished\":\"2005\"}");
+    }
+}
